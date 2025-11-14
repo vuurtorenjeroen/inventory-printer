5cb3863b0cc552fbbae7959c7182a8ccf6ec2d69
Jeroen Stroeve <jeroen@stroevesoer.nl>
2025-11-14 19:16:59 +0100

    Add location QR and portrait labels

--- labels.py
+++ labels.py
@@ -65,3 +65,45 @@ def item_qrcode(data):
     printer = get_printer(labelx, labely)
     finish_label(pdf, printer)
 
+
+# Location default
+def location_default(data):
+    location_portrait(data)
+
+
+# Location QR (Dymo 30332 / S0929120)
+def location_qrcode(data):
+    item_qrcode(data)
+
+
+# Location portrait (Dymo 99014 / S0722430)
+def location_portrait(data):
+    labelx = 54
+    labely = 101
+    pdf = start_label(labelx, labely)
+
+    qrsize = 33
+
+    pdf.set_y(14)
+    pdf.set_font(size=68)
+    pdf.cell(text=f"**{data['name']}**", markdown=True, center=True, align="C")
+
+    x = (labelx - qrsize - 1) / 2
+    pdf.set_line_width(0.4)
+    pdf.line(x1=x, y1=41, x2=labelx-x, y2=41)
+
+    pdf.set_y(45)
+    pdf.set_font(size=12)
+    pdf.cell(text=f"**{data['contents']}**", markdown=True, center=True, align="C")
+    pdf.set_y(51)
+    pdf.cell(text=f"**{data['id']}**", markdown=True, center=True, align="C")
+
+    x = (labelx - qrsize - 1) / 2
+    pdf.set_line_width(0.3)
+    pdf.line(x1=x, y1=59, x2=labelx-x, y2=59)
+
+    img = qrcode.make(data["id"], border=0)
+    pdf.image(img.get_image(), x=((labelx-qrsize)/2), y=61, w=qrsize)
+
+    printer = get_printer(labelx, labely)
+    finish_label(pdf, printer)
